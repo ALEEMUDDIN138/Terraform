e0cbce91556a0da2eb95220f74e9f874a7ee34aa
ALEEMUDDIN138 <aleemuddin138@gmail.com>
2025-09-10 16:20:18 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,11 @@
 variable "filename" {
   description = "The name of the file"
   type        = string
+  default     = "book.txt"
 }
+
 variable "content" {
   description = "The content of the file"
   type        = string
+  default     = "Hello from Terraform!"
 }
