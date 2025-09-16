9ed920d26af5443a575d55477491aa563dd6f291
ALEEMUDDIN138 <aleemuddin138@gmail.com>
2025-09-16 15:46:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
-resource "local_file" "my_work" {
-filename=var.filename
-content=var.content
-}  
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.3.0"
+    }
+  }
+}
+
+variable "filename" {
+  type    = list(string)
+  default = ["Apple1.txt", "Banana2.txt", "Mango3.txt"]
+}
+
+resource "local_file" "pet" {
+  for_each = toset(var.filename)
+  filename = each.value
+  content  = "Hello Terraform"
+}
