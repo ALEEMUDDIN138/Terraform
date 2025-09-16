edad27638de46f834debf619908d2c3bee24fd3f
ALEEMUDDIN138 <aleemuddin138@gmail.com>
2025-09-16 16:04:33 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,7 @@
 variable "filename" {
-  default = [
+  description = "List of file names to create"
+  type        = list(string)
+  default     = [
     "Apple.txt",
     "Banana.txt",
     "Mango.txt"
